8b5901450ea6e3738c3e80ac24921a556c8890b5
srikant2411 <30661560+srikant2411@users.noreply.github.com>
2017-08-07 00:02:27 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('lolollllda');
 //chagethetextofmai-textfilediv
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+var element=document.getElementById('katta');
+katta.onclick =fuction()
+{
+    img.style.marginLeft='100px';
+};
+//element.innerHTML='New value';
+
+//movetoimade
