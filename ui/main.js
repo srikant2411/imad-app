b87720793abf762e063db8000a583744f2d1ee32
srikant2411 <30661560+srikant2411@users.noreply.github.com>
2017-08-07 00:10:54 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 
 var img = document.getElementById('katta');
-katta.onclick =fuction(){
+img.onclick = fuction();{
     img.style.marginLeft='100px';
 }
 //element.innerHTML='New value';
